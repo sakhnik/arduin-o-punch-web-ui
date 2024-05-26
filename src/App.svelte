2bfa8c75e0e08bb51b3b7149d5cf336048d83331
Anatolii Sakhnik <sakhnik@gmail.com>
2024-05-26 21:01:48 +0300

    Add a popup component

--- src/App.svelte
+++ src/App.svelte
@@ -7,9 +7,16 @@
 <script>
 import Settings from './settings.svelte'
 import Upgrade from './upgrade.svelte'
+import Popup from './popup.svelte'
 
 let project_version = '1.0.0'
 let git_revision = 'abcdef'
+let recordShown = false
+
+function openRecord() {
+  recordShown = false
+  recordShown = true
+}
 </script>
 
 <div class='container'>
@@ -17,5 +24,7 @@ let git_revision = 'abcdef'
   <p>Версія <span>{project_version}-{git_revision}</span></p>
 
   <Settings/>
+  <Popup show={recordShown}/>
+  <button on:click={openRecord}>Журнал</button>
   <Upgrade/>
 </div>
---
+++ src/popup.svelte
@@ -0,0 +1,44 @@
+<style>
+.popup {
+  display: none;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  justify-content: center;
+  align-items: center;
+}
+
+.show {
+  display: flex;
+}
+
+.popup-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 8px;
+  text-align: center;
+}
+
+#closeBtn {
+    margin-top: 20px;
+}
+</style>
+
+<script>
+
+export let show = false
+
+</script>
+
+<div id="popup" class="popup" class:show={show}>
+  <div class="popup-content">
+    <button id="closeBtn" on:click={() => show = false}>Закрити</button>
+    <slot>
+      <p>Popup content</p>
+    </slot>
+  </div>
+</div>
+<svelte:window on:keydown={() => show = false} />
