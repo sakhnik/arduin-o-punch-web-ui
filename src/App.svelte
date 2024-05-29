ee2235a7cc003c9d6061a709f08ef9be1e32d958
Anatolii Sakhnik <sakhnik@gmail.com>
2024-05-29 19:32:47 +0300

    Bundle for arduin-o-punch #3

--- src/App.svelte
+++ src/App.svelte
@@ -10,14 +10,12 @@ import Upgrade from './upgrade.svelte'
 import Popup from './popup.svelte'
 import Record from './record.svelte'
 
-let project_version = '1.0.0'
-let git_revision = 'abcdef'
 let recordShown = false
 </script>
 
 <div class='container'>
   <h1>Станція Arduin-O-Punch</h1>
-  <p>Версія <span>{project_version}-{git_revision}</span></p>
+  <p>Версія <span>0.0.0-abcdef</span></p>
 
   <Settings/>
   <Popup bind:isOpen={recordShown}>
